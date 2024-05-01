045e8e66309b7c106367dd9fb052c1c7c4ea7013
ufms-facility <168665498+ufms-facility@users.noreply.github.com>
2024-05-01 18:01:22 -0300

    Começo do Projeto

---
+++ src/main/java/br/com/facility/FacilityApplication.java
@@ -0,0 +1,13 @@
+package br.com.facility;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FacilityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FacilityApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/facility/FacilityApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.facility;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FacilityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
