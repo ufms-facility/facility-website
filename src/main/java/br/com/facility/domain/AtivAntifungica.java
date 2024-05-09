5a0d866d521b88a316757a715f1581774358f5d2
ufms-facility <168665498+ufms-facility@users.noreply.github.com>
2024-05-09 15:41:00 -0300

    Salvo do ultimo update

--- src/main/java/br/com/facility/domain/AtivAntifungica.java
+++ src/main/java/br/com/facility/domain/AtivAntifungica.java
@@ -1,5 +1,56 @@
 package br.com.facility.domain;
 
-public class AtivAntifungica {
+import java.io.Serializable;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+
+@Entity
+public class AtivAntifungica implements Serializable {
+	private static final long serialVersionUID = 1L;
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String ativAntiFungDesc;
+	
+	@ManyToOne
+	@JoinColumn(name = "id_peptideo", nullable = false, updatable = true)
+	private Peptideo peptideo;
+	
+	public AtivAntifungica() {
+		
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getAtivAntiFungDesc() {
+		return ativAntiFungDesc;
+	}
+
+	public void setAtivAntiFungDesc(String ativAntiFungDesc) {
+		this.ativAntiFungDesc = ativAntiFungDesc;
+	}
+
+	public Peptideo getPeptideo() {
+		return peptideo;
+	}
+
+	public void setPeptideo(Peptideo peptideo) {
+		this.peptideo = peptideo;
+	}
+
+	public static long getSerialversionuid() {
+		return serialVersionUID;
+	}
 
 }
--- src/main/java/br/com/facility/domain/FuncBiologica.java
+++ src/main/java/br/com/facility/domain/FuncBiologica.java
@@ -1,5 +1,56 @@
 package br.com.facility.domain;
 
-public class FuncBiologica {
+import java.io.Serializable;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+
+@Entity
+public class FuncBiologica implements Serializable {
+	private static final long serialVersionUID = 1L;
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String funcBiolDesc;
+	
+	@ManyToOne
+	@JoinColumn(name = "id_peptideo", nullable = false, updatable = true)
+	private Peptideo peptideo;
+	
+	public FuncBiologica() {
+		
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getFuncBiolDesc() {
+		return funcBiolDesc;
+	}
+
+	public void setFuncBiolDesc(String funcBiolDesc) {
+		this.funcBiolDesc = funcBiolDesc;
+	}
+
+	public Peptideo getPeptideo() {
+		return peptideo;
+	}
+
+	public void setPeptideo(Peptideo peptideo) {
+		this.peptideo = peptideo;
+	}
+
+	public static long getSerialversionuid() {
+		return serialVersionUID;
+	}
+	
 }
