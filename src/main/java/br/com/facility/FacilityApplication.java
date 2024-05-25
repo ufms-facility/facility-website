27e467bc513a81f79a1d7f7a8f5ee9cde3d2ab95
ufms-facility <168665498+ufms-facility@users.noreply.github.com>
2024-05-25 16:27:11 -0300

    FacilityApplication Allowed Origins

--- src/main/java/br/com/facility/FacilityApplication.java
+++ src/main/java/br/com/facility/FacilityApplication.java
@@ -2,6 +2,9 @@ package br.com.facility;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class FacilityApplication {
@@ -10,4 +13,13 @@ public class FacilityApplication {
 		SpringApplication.run(FacilityApplication.class, args);
 	}
 
+	@Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**").allowedOrigins("http://200.130.152.123");
+            }
+        };
+    }
 }
