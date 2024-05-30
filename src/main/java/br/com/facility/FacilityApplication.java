d7a60ece94a41afed386a4cde86dd3c88bd87f2c
caio <hi@caio.sh>
2024-05-30 09:54:58 -0300

    sqlite

--- src/main/java/br/com/facility/FacilityApplication.java
+++ src/main/java/br/com/facility/FacilityApplication.java
@@ -1,5 +1,9 @@
 package br.com.facility;
 
+import javax.sql.DataSource;
+import org.springframework.core.env.Environment;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
@@ -18,8 +22,23 @@ public class FacilityApplication {
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**").allowedOrigins("*");
+                registry
+					.addMapping("/**").allowedOrigins("*");
+                registry.addMapping("/**").allowedMethods("*");
             }
         };
     }
+
+	@Autowired
+	private Environment env;
+
+	@Bean
+	public DataSource dataSource() {
+		final DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setDriverClassName(env.getProperty("driverClassName"));
+        dataSource.setUrl(env.getProperty("url"));
+        dataSource.setUsername(env.getProperty("user"));
+        dataSource.setPassword(env.getProperty("password"));
+        return dataSource;
+	}
 }
